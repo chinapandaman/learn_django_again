ed246d576572451346819a9be0d528ce7ed44b7b
chinapandaman <b.j.li@foxmail.com>
2022-02-21 14:31:23 -0600

    quickstart

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("polls/", include("polls.urls")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('admin/', admin.site.urls),
 ]
